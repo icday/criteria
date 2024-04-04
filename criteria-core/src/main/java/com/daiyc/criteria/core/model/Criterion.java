5268173320fb49395ae6160fb756230906943026
icday <ycdai33@gmail.com>
2024-04-04 15:59:04 +0800

    refactor: remove useless code

--- criteria-core/src/main/java/com/daiyc/criteria/core/model/Criterion.java
+++ criteria-core/src/main/java/com/daiyc/criteria/core/model/Criterion.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author daiyc
@@ -29,26 +28,6 @@ public class Criterion<T> implements Element {
 
     public Criterion(String fieldName, OperatorEnum operator, List<T> listValue) {
         this(fieldName, operator, null, listValue);
-    }
-
-    @Override
-    public String toString() {
-        OperandNum operandNum = operator.getOperandNum();
-        if (operandNum == OperandNum.NONE) {
-            return String.format("%s %s", fieldName, operator.getSymbol());
-        } else if (operandNum == OperandNum.SINGLE) {
-            return String.format("%s %s %s", fieldName, operator.getSymbol(), singleValue);
-        } else if (operandNum == OperandNum.DOUBLE) {
-            int size = listValue.size();
-            return String.format("%s %s (%s, %s)", fieldName, operator.getSymbol()
-                    , size < 1 ? null : listValue.get(0)
-                    , size < 2 ? null : listValue.get(1)
-            );
-        } else {
-            return String.format("%s %s (%s)", fieldName, operator.getSymbol()
-                    , listValue.stream().map(Object::toString).collect(Collectors.joining(", "))
-            );
-        }
     }
 
     @Override
